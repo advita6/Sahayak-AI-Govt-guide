2fa0f8bc8b55737a4bfcddd6681af3f7c280a14c
Advita <advita@gmail.com>
2026-03-16 23:16:02 +0530

    Keep only README in main branch

--- backend/routes/assistantRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const assistantController = require('../controllers/assistantController');
-
-router.post('/ask', assistantController.askQuestion);
-router.get('/forms', assistantController.getForms);
-
-module.exports = router;
--- backend/routes/guideRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const guideController = require('../controllers/guideController');
-
-router.get('/', guideController.getAllGuides);
-router.get('/:formId', guideController.getGuideById);
-router.get('/:formId/steps', guideController.getGuideSteps);
-
-module.exports = router;
